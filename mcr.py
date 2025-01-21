799488b41aa6d79dc1d34a63c7b1b7e53d5f7173
shawnyan-01 <85020496+shawnyan-01@users.noreply.github.com>
2025-01-21 17:18:39 +0800

    Update mcr.py

--- mcr.py
+++ mcr.py
@@ -1,55 +1,55 @@
-def is_win(game):
+def is_win(board):
     win = False
     # Check rows
-    if game[0][0] == game[0][1] == game[0][2] and (game[0][0] == 'X' or game[0][0] == 'O'):
+    if board[0][0] == board[0][1] == board[0][2] and (board[0][0] == 'X' or board[0][0] == 'O'):
         win = True
-    if game[1][0] == game[1][1] == game[1][2] and (game[1][0] == 'X' or game[1][0] == 'O'):
+    if board[1][0] == board[1][1] == board[1][2] and (board[1][0] == 'X' or board[1][0] == 'O'):
         win = True
-    if game[2][0] == game[2][1] == game[2][2] and (game[2][0] == 'X' or game[2][0] == 'O'):
+    if board[2][0] == board[2][1] == board[2][2] and (board[2][0] == 'X' or board[2][0] == 'O'):
         win = True
     # Check columns
-    if game[0][0] == game[1][0] == game[2][0] and (game[0][0] == 'X' or game[0][0] == 'O'):
+    if board[0][0] == board[1][0] == board[2][0] and (board[0][0] == 'X' or board[0][0] == 'O'):
         win = True
-    if game[0][1] == game[1][1] == game[2][1] and (game[0][1] == 'X' or game[0][1] == 'O'):
+    if board[0][1] == board[1][1] == board[2][1] and (board[0][1] == 'X' or board[0][1] == 'O'):
         win = True
-    if game[0][2] == game[1][2] == game[2][2] and (game[0][2] == 'X' or game[0][2] == 'O'):
+    if board[0][2] == board[1][2] == board[2][2] and (board[0][2] == 'X' or board[0][2] == 'O'):
         win = True
     # Check diagonals
-    if game[0][0] == game[1][1] == game[2][2] and (game[0][0] == 'X' or game[0][0] == 'O'):
+    if board[0][0] == board[1][1] == board[2][2] and (board[0][0] == 'X' or board[0][0] == 'O'):
         win = True
-    if game[0][2] == game[1][1] == game[2][0] and (game[0][2] == 'X' or game[0][2] == 'O'):
+    if board[0][2] == board[1][1] == board[2][0] and (board[0][2] == 'X' or board[0][2] == 'O'):
         win = True
     return win
 
 def main():
-    game = [[' ' for _ in range(3)] for _ in range(3)]  # Tic-tac-toe board
-    player1 = 'X'
-    player2 = 'O'
-    turn = False  # False for player 1's turn, True for player 2's turn. Player 1 first.
+    board = [[' ' for _ in range(3)] for _ in range(3)]  # Tic-tac-toe board
+    player1_symbol = 'X'
+    player2_symbol = 'O'
+    is_player2_turn = False  # False for Player 1's turn, True for Player 2's turn. Player 1 starts.
     print("X = Player 1")
     print("O = Player 2")
-    for n in range(9):
-        turn = not turn  # Switch turns
-        if not turn:
+    for turn_count in range(9):
+        is_player2_turn = not is_player2_turn  # Switch turns
+        if not is_player2_turn:
             print("Player 1: ", end="")
         else:
             print("Player 2: ", end="")
         print("Which cell to mark? i:[1..3], j:[1..3]: ")
-        i, j = map(int, input().split())
-        i -= 1
-        j -= 1
-        if not turn:
-            game[i][j] = 'X'
+        row, col = map(int, input().split())
+        row -= 1
+        col -= 1
+        if not is_player2_turn:
+            board[row][col] = player1_symbol
         else:
-            game[i][j] = 'O'
-        if is_win(game):
+            board[row][col] = player2_symbol
+        if is_win(board):
             print("Win!")
             break  # Terminate the game
-        if n == 8:  # All cells have been filled
+        if turn_count == 8:  # All cells have been filled
             print("Tie!")
         # Show the game board
-        for row in game:
+        for row in board:
             print(" ".join(row))
-# test commit
+
 if __name__ == "__main__":
     main()
